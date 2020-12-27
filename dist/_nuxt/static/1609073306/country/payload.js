2b1a6b72608f4f57f1f582dd58f24949036592a8
logotip4ik <bogdankostyuk12@gmail.com>
2020-12-27 15:01:09 +0200

    update: published to vercel

--- dist/_nuxt/static/1609073306/country/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/country", {data:[{}],fetch:[],mutations:[]});
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  router: {
-    base: '/countries',
-  },
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'countries',
