bec6aa56fd00dcedac11df208db4d58dd86f04b8
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-03-13 14:37:32 +0200

    update: removed global scoped css from layout component

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,30 +19,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;800&display=swap');
-@import '@/assets/scss/variables';
-
-*,
-*::before,
-*::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  transition: background 200ms ease-in;
-}
-
-html,
-body {
-  font-family: 'Nunito', sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  background: $very-light-gray;
-  min-height: 100vh;
-
-  &.dark {
-    background: $very-dark-blue;
-  }
-}
-</style>
